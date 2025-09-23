3e3af7978e2e02b86fdb1da2fc73dd127e4517b5
Charan Aravind <charan27072005@gmail.com>
2025-09-23 22:33:00 +0530

    modified routers

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,20 +8,17 @@ import vector_activity_routes from "./routes/vector_activity_routes.js";
 import { transcribeRequestAudio } from "./services/speech_text.js";
 const router = express.Router();
 
+// Multer setup
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 router.use("/farmer",user_router)
 router.use("/activity",activity_router)
 router.use("/crop",crop_router)
 router.use("/vector",vector_activity_routes)
-export default router
-router.use("/farmer", user_router);
-router.use("/activity", activity_router);
-router.use("/crop", crop_router);
-
-// Multer setup
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
 router.post("/speech_text", upload.single("audio"), transcribeRequestAudio);
+
+
+
 
 export default router;
