f648621dfe6937f3a27027a2e25eba49b004497e
Aditya Varma Chandika <adithyavarmachandika@gmail.com>
2025-09-24 07:18:03 +0530

    fetch user queries

--- backend/src/services/twilo.js
+++ backend/src/services/twilo.js
@@ -7,7 +7,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
-export async function create_message(body, phone_number) {
+export async function createMessage(body, phone_number) {
 
     phone_number= "+91"+phone_number
     console.log(accountSid,authToken)
