7eee48c6787266a1da7bcada419cd189b64df190
Charan Aravind <charan27072005@gmail.com>
2025-09-29 21:55:03 +0530

    translation service in backend

--- backend/src/controllers/query_controller.js
+++ backend/src/controllers/query_controller.js
@@ -56,6 +56,8 @@ Make the answer brief and concise. If unsure, just say "I don't know". Do not fa
 
     const geminiResponse = await chatWithGemini(systemPrompt);
 
+
+   
     newQuery.answer = geminiResponse;
     await newQuery.save();
 
